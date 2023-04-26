6b96292b011e71cf4c7bdb19fffa75cec6aa465a
Raghu-KV <raghunandanv19@gmail.com>
2023-04-26 16:09:58 +0530

    i

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import SingleProduct from "./SingleProduct";
+import { products } from "./../product";
+
 function ProductList() {
   return (
     <div className="row g-4">
-      <SingleProduct />
-      <SingleProduct />
-      <SingleProduct />
-      <SingleProduct />
-      <SingleProduct />
-      <SingleProduct />
-      <SingleProduct />
-      <SingleProduct />
+      {products.map((product) => (
+        <SingleProduct />
+      ))}
     </div>
   );
 }
