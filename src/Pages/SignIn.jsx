22dd8842ea9fec1bd58f3b4bc62db0746c9359c2
Raghu-KV <raghunandanv19@gmail.com>
2023-05-05 12:59:31 +0530

    added formik and yup

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -1,7 +1,27 @@
+import { useFormik } from "formik";
+import * as yup from "yup";
+
 function SignIn() {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: yup.object({
+      email: yup
+        .string()
+        .required("Emil is Required")
+        .email("should be a vailed email"),
+      password: yup.string().required("passwored is required"),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
   return (
     <div>
-      <form>
+      <form onSubmit={formik.handleSubmit}>
         <div className="mb-3">
           <label for="exampleInputEmail1" className="form-label">
             Email address
@@ -11,10 +31,16 @@ function SignIn() {
             className="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
+            name="email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
           />
-          <div id="emailHelp" className="form-text text-danger">
-            error message
-          </div>
+          {formik.touched.email && formik.errors.email ? (
+            <div id="emailHelp" className="form-text text-danger">
+              {formik.errors.email}
+            </div>
+          ) : null}
         </div>
         <div className="mb-3">
           <label for="exampleInputPassword1" className="form-label">
@@ -24,13 +50,23 @@ function SignIn() {
             type="password"
             className="form-control"
             id="exampleInputPassword1"
+            name="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
           />
-          <div id="emailHelp" className="form-text text-danger">
-            error message
-          </div>
+          {formik.touched.password && formik.errors.password ? (
+            <div id="emailHelp" className="form-text text-danger">
+              {formik.errors.password}
+            </div>
+          ) : null}
         </div>
 
-        <button type="submit" className="btn btn-primary">
+        <button
+          type="submit"
+          className="btn btn-primary"
+          disabled={formik.isSubmitting}
+        >
           Submit
         </button>
       </form>
