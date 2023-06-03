fc2f3368f53514e822795025704a501a8e81dd1d
Raghu-KV <raghunandanv19@gmail.com>
2023-06-03 19:30:29 +0530

    switch

--- src/formikComponents/FormikCountrol.jsx
+++ src/formikComponents/FormikCountrol.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from "react";
 
-function FormikCountrol({control}) {
-switch(control){
+function FormikCountrol({ control }) {
+  switch (control) {
     case "input":
-    case:"text"    
+    case "textarea":
+    case "select":
+    case "radio":
+    case "date":
+  }
+
+  return <div>FormikCountrol</div>;
 }
 
-  return (
-    <div>FormikCountrol</div>
-  )
-}
-
-export default FormikCountrol
+export default FormikCountrol;
