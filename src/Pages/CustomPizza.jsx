d70a61d240373b3294fe45a526975c438e96b07b
Raghu-KV <raghunandanv19@gmail.com>
2023-06-16 15:50:22 +0530

    custom pizza

--- src/Pages/CustomPizza.jsx
+++ src/Pages/CustomPizza.jsx
@@ -1,12 +1,11 @@
-import { useFormik } from "formik";
-import { Link } from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as yup from "yup";
 import { BACK_END_URL } from "../URL";
-import { useState } from "react";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { addCustomPizzaData } from "../redux_reducers/customPizza";
+import DeleteTextError from "../components/DeleteTextError";
 
 function CustomPizza() {
   const dispatch = useDispatch();
@@ -24,30 +23,78 @@ function CustomPizza() {
 
   const customPizzaData = useSelector((state) => state.customPizza.value);
 
-  const [
-    allPizzaBasesObj,
-    allPizzaSaucesObj,
-    allPizzaCheeseObj,
-    allVeggiesObj,
-    allMeatObj,
-  ] = customPizzaData;
+  useEffect(() => {
+    if (!customPizzaData.length === 0) {
+      const [
+        allPizzaBasesObj,
+        allPizzaSaucesObj,
+        allPizzaCheeseObj,
+        allVeggiesObj,
+        allMeatObj,
+      ] = customPizzaData;
 
-  const { allPizzaBases } = allPizzaBasesObj;
-  const { allPizzaSauces } = allPizzaSaucesObj;
-  const { allPizzaCheese } = allPizzaCheeseObj;
-  const { allVeggies } = allVeggiesObj;
-  const { allMeat } = allMeatObj;
+      const { allPizzaBases } = allPizzaBasesObj;
+      const { allPizzaSauces } = allPizzaSaucesObj;
+      const { allPizzaCheese } = allPizzaCheeseObj;
+      const { allVeggies } = allVeggiesObj;
+      const { allMeat } = allMeatObj;
 
-  console.log(
-    "this is the one",
-    allPizzaBases,
-    allPizzaSauces,
-    allPizzaCheese,
-    allVeggies,
-    allMeat
+      console.log(
+        "this is the one",
+        allPizzaBases,
+        allPizzaSauces,
+        allPizzaCheese,
+        allVeggies,
+        allMeat
+      );
+    }
+  }, [customPizzaData]);
+
+  const initialValues = {
+    test: "",
+  };
+
+  const validationSchema = yup.object({
+    test: yup.string().required("this is test and it is required"),
+  });
+
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      {(props) => {
+        const { isSubmitting } = props;
+        return (
+          <Form>
+            <div className="mb-3">
+              <label htmlFor="testForm" className="form-label">
+                enter Test
+              </label>
+              <Field
+                name="test"
+                id="testForm"
+                className="form-control border border-black"
+              />
+              <ErrorMessage name="test" component={DeleteTextError} />
+            </div>
+            <button
+              type="submit"
+              className="btn btn-primary"
+              disabled={isSubmitting}
+            >
+              Add to Cart
+            </button>
+          </Form>
+        );
+      }}
+    </Formik>
   );
-
-  return <div>custom pizza</div>;
 }
 
 export default CustomPizza;
