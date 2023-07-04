630bbccc1d8b60692e38e88a4aaeb736e88167b6
Raghu-KV <raghunandanv19@gmail.com>
2023-07-04 20:21:50 +0530

    changed backend url

--- src/URL.js
+++ src/URL.js
@@ -1,2 +1,2 @@
 export const FRONT_END_URL = "https://tiny-fairy-95fba1.netlify.app";
-export const BACK_END_URL = "http://localhost:4000";
+export const BACK_END_URL = "https://pizza-delivery-back-end.vercel.app";
